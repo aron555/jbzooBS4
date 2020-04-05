06c76fcbcbf5b358154093b55ef7d61c5e4c7862
Ivan Aronov <aron555@inbox.ru>
2020-04-05 16:45:29 +0300

    pl-0 in category name

--- media/zoo/applications/jbuniversal/templates/bootstrap4/renderer/category/_default.php
+++ media/zoo/applications/jbuniversal/templates/bootstrap4/renderer/category/_default.php
@@ -26,7 +26,7 @@ $image    = $this->app->jbimage->get('category_image', $vars['params']);
 $title    = $title ? $title : $category->name;
 
 if ((int)$vars['params']->get('template.category_show', 1)) : ?>
-    <div class="category pt-0 alias-<?php echo $category->alias; ?> ">
+    <div class="category pt-0 pl-0 alias-<?php echo $category->alias; ?> ">
 
         <?php if ((int)$vars['params']->get('template.category_title_show', 1)) : ?>
             <h1 class="title products-cat-title"><?php echo $title; ?></h1>
